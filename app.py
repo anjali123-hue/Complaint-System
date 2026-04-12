bbfc285310628e6745490c4a6330fcc2af763964
Anjali Yadav <anjaliy0805@gmail.com>
2026-04-13 03:25:44 +0530

    Updated project files now only

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -99,4 +100,5 @@ def delete(id):
     return redirect('/admin')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
